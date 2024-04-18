0cfaf0dc440878575a2ef1f858ce8bbd96249d17
sharjilansari <sharjilansari234@gmail.com>
2024-04-18 19:09:14 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
         <header className="header">
             <img src={trollFace} className="header--image" />
             <h2 className="header--title">MEME GENERATOR</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project"></h4>
         </header>
     )
 }
